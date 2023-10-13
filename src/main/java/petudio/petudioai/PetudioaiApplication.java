732275ebc50976dc45e4b9295db8a27d3ea91930
DDongYul <comeme0101@naver.com>
2023-10-13 15:10:49 +0900

    initial commit

---
+++ src/main/java/petudio/petudioai/PetudioaiApplication.java
@@ -0,0 +1,13 @@
+package petudio.petudioai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetudioaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetudioaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/petudio/petudioai/PetudioaiApplicationTests.java
@@ -0,0 +1,13 @@
+package petudio.petudioai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetudioaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
