e35e906193ee6d9b53b8bbfa84170a11bbf6bc37
DDongYul <comeme0101@naver.com>
2023-10-13 15:47:10 +0900

    패키지 경로 수정

---
+++ src/main/java/petudio/petudioai/controller/testController.java
@@ -0,0 +1,20 @@
+package petudio.petudioai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@RestController
+public class testController {
+
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+
+}
