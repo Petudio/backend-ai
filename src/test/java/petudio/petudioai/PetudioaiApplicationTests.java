bac3379c964e94a34d3f271b80d73cd1e69d2bc5
DDongYul <comeme0101@naver.com>
2023-10-13 15:24:58 +0900

    remove SpringBootTest

--- src/test/java/petudio/petudioai/PetudioaiApplicationTests.java
+++ src/test/java/petudio/petudioai/PetudioaiApplicationTests.java
@@ -3,11 +3,11 @@ package petudio.petudioai;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PetudioaiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
