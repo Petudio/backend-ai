0ae3293d6a81b5503159851e4c994645c1f6ca61
DDongYul <comeme0101@naver.com>
2023-10-13 16:09:08 +0900

    Feat: ProcessBuilder test

--- src/main/java/petudio/petudioai/controller/testController.java
+++ src/main/java/petudio/petudioai/controller/testController.java
@@ -12,9 +12,21 @@ import java.io.InputStreamReader;
 @RestController
 public class testController {
 
-    @RequestMapping("/hello")
-    public String Hello(){
+    @GetMapping("/hello")
+    public String Hello() throws IOException {
+        String pythonPath = "/Users/dongyullee/IntelliJ/petudioai/src/main/java/petudio/petudioai/python/test.py";
+        ProcessBuilder processBuilder = new ProcessBuilder("python", pythonPath);
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
         return "hello";
     }
 
+    @GetMapping("/wow")
+    public String Wow() {
+        return "wow";
+    }
 }
